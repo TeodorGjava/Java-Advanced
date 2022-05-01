f6b2ab87ba0be7f1653e33b039483af1c1aae578
TeodorGjava <90547780+TeodorGjava@users.noreply.github.com>
2022-05-01 11:40:07 +0300

    Stacks

---
+++ src/StacksAndQueues/DecimalToBinary.java
@@ -0,0 +1,20 @@
+package StacksAndQueues;
+
+import java.util.ArrayDeque;
+import java.util.Scanner;
+
+public class DecimalToBinary {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        ArrayDeque<Integer> stack = new ArrayDeque<>();
+        int input = Integer.parseInt(sc.nextLine());
+        while (input!=0){
+            stack.push(input%2);
+            input/=2;
+        }
+        int n = stack.size();
+        for (int i = 0; i <n ; i++) {
+            System.out.print(stack.pop());
+        }
+    }
+}
