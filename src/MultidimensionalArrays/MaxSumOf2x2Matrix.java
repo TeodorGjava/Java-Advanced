e384c78bd8328acb87b30c603ed367e34bac9489
TeodorGjava <90547780+TeodorGjava@users.noreply.github.com>
2022-05-13 19:38:37 +0300

    Create MaxSumOf2x2Matrix.java

---
+++ src/MultidimensionalArrays/MaxSumOf2x2Matrix.java
@@ -0,0 +1,10 @@
+package MultidimensionalArrays;
+
+import java.util.Scanner;
+
+public class MaxSumOf2x2Matrix {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+    }
+}
