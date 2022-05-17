e561d4e61ac0a62322aab4814ca9f8ba728d0f09
TeodorGjava <90547780+TeodorGjava@users.noreply.github.com>
2022-05-17 20:09:57 +0300

    Update DecimalToBinary.java

--- src/StacksAndQueues/DecimalToBinary.java
+++ src/StacksAndQueues/DecimalToBinary.java
@@ -8,6 +8,7 @@ public class DecimalToBinary {
         Scanner sc = new Scanner(System.in);
         ArrayDeque<Integer> stack = new ArrayDeque<>();
         int input = Integer.parseInt(sc.nextLine());
+        if(input!=0){
         while (input!=0){
             stack.push(input%2);
             input/=2;
@@ -16,5 +17,7 @@ public class DecimalToBinary {
         for (int i = 0; i <n ; i++) {
             System.out.print(stack.pop());
         }
-    }
+    }else{
+            System.out.println(0);
+        }}
 }
