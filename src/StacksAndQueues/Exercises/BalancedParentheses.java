8dac7d0112b3120ea24d34d0dc83ea38ae455ddf
Teodor Georgiev <tgeorgievmk2@gmail.com>
2022-05-20 11:41:42 +0300

    Create BalancedParentheses.java

---
+++ src/StacksAndQueues/Exercises/BalancedParentheses.java
@@ -0,0 +1,11 @@
+package StacksAndQueues.Exercises;
+
+import java.util.Scanner;
+
+public class BalancedParentheses {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+
+    }
+}
