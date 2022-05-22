5cbfd240da5077f9d65784197a1119434e645afd
TeodorGjava <90547780+TeodorGjava@users.noreply.github.com>
2022-05-22 17:30:44 +0300

    Create Fibonacci.java

---
+++ src/StacksAndQueues/Exercises/Fibonacci.java
@@ -0,0 +1,31 @@
+package StacksAndQueues.Exercises;
+
+import java.util.ArrayDeque;
+import java.util.Scanner;
+
+public class Fibonacci {
+    private static long[] memory;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = Integer.parseInt(sc.nextLine());
+        memory=new long[n+1];
+        if(n==0){
+            System.out.println(0);
+            return;
+        }
+       long result = fib(n);
+        System.out.println(result);
+    }
+
+    private static long fib(int n) {
+        if (n <= 1) {
+            return 1l;
+        }
+        if(memory[n]!=0){
+            return memory[n];
+        }
+        memory[n]= fib(n-1)+fib(n-2);
+        return memory[n];
+    }
+}
