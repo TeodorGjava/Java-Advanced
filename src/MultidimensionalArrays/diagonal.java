8a6a2b7e22088082e0bbe5d199c0c3e71fbde651
TeodorGjava <90547780+TeodorGjava@users.noreply.github.com>
2022-05-23 20:33:18 +0300

    Update diagonal.java

--- src/MultidimensionalArrays/diagonal.java
+++ src/MultidimensionalArrays/diagonal.java
@@ -6,18 +6,18 @@ import java.util.Scanner;
 public class diagonal {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = Integer.parseInt(sc.nextLine());
-        int[][] matrix = new int[n][n];
-        for (int row = 0; row < n; row++) {
+        int size = Integer.parseInt(sc.nextLine());
+        int[][] matrix = new int[size][size];
+        for (int row = 0; row < size; row++) {
             int[] arr = Arrays.stream(sc.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-            System.arraycopy(arr, 0, matrix[row], 0, n);
+            System.arraycopy(arr, 0, matrix[row], 0, size);
         }
-        for (int row = 0; row < n; row++) {
+        for (int row = 0; row < size; row++) {
             System.out.print(matrix[row][row] + " ");
         }
         System.out.println();
         int i = 0;
-        for (int col = n - 1; col >= 0; col--, i++) {
+        for (int col = size - 1; col >= 0; col--, i++) {
             System.out.print(matrix[col][i] + " ");
         }
     }
