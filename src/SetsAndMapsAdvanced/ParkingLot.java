cb216fa28b2625c1df4d303ed1d302646fa12db0
TeodorGjava <90547780+TeodorGjava@users.noreply.github.com>
2022-05-25 18:55:03 +0300

    create ParkingLot

---
+++ src/SetsAndMapsAdvanced/ParkingLot.java
@@ -0,0 +1,35 @@
+package SetsAndMapsAdvanced;
+
+import java.util.LinkedHashSet;
+import java.util.Scanner;
+
+public class ParkingLot {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] command = sc.nextLine().split(", ");
+        LinkedHashSet<String> parking = new LinkedHashSet<>();
+        int i = 0;
+        while (!command[0].equals("END")) {
+            String number = command[1];
+            if (command[0].equals("IN")) {
+                parking.add(number);
+            } else {
+                if (!parking.isEmpty()) {
+                    parking.remove(number);
+                } else {
+                    System.out.println("Parking Lot is Empty");
+                }
+            }
+            i++;
+            command = sc.nextLine().split(", ");
+        }
+        if (!parking.isEmpty()) {
+            for (String car : parking
+            ) {
+                System.out.println(car);
+            }
+        } else {
+            System.out.println("Parking Lot is Empty");
+        }
+    }
+}
