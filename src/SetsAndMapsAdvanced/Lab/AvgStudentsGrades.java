032127eb173a30caadfeea6bfb6c28dd0b21bd21
TeodorGjava <90547780+TeodorGjava@users.noreply.github.com>
2022-05-29 16:03:28 +0300

    Create AvgStudentsGrades.java

---
+++ src/SetsAndMapsAdvanced/Lab/AvgStudentsGrades.java
@@ -0,0 +1,28 @@
+package SetsAndMapsAdvanced.Lab;
+
+import java.util.*;
+
+public class AvgStudentsGrades {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int records = Integer.parseInt(sc.nextLine());
+        Map<String, List<Double>> map = new TreeMap<>();
+        for (int i = 0; i < records; i++) {
+            String[] input = sc.nextLine().split(" ");
+            map.putIfAbsent(input[0], new ArrayList<>());
+            if (map.containsKey(input[0])) {
+                map.get(input[0]).add(Double.parseDouble(input[1]));
+            }
+        }
+        map.forEach((key, value) -> {
+            final double[] sum = {0};
+            System.out.print(key + " -> ");
+            value.stream().forEach(x -> System.out.printf("%.2f ",x));
+            value.stream().forEach(x-> sum[0] +=x);
+            System.out.printf("(avg: %.2f)%n", (double) sum[0] / value.size());
+            sum[0]=0;
+
+
+        });
+    }
+}
