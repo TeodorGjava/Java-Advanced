3190ca786fcb7bb67f7f68a4239deb50e0033f13
TeodorGjava <90547780+TeodorGjava@users.noreply.github.com>
2022-05-29 20:49:23 +0300

    Update copyBytes.java

--- src/FilesAndStreams/Lab/copyBytes.java
+++ src/FilesAndStreams/Lab/copyBytes.java
@@ -1,28 +1,26 @@
 package FilesAndStreams.Lab;
 
 import java.io.*;
-import java.util.Scanner;
-import java.util.Set;
 
 public class copyBytes {
     public static void main(String[] args) throws IOException {
         String path = "D:\\Projects\\Java-Advanced-Files-and-Streams-Lab-Resources\\Files-and-Streams\\input.txt";
         FileInputStream file = new FileInputStream(path);
-        FileOutputStream out = new FileOutputStream("output.txt");
-        int bytte = file.read();
-        while (bytte != -1) {
-            String digits = String.valueOf(bytte);
-            if(bytte==' '){
-                out.write((char)32);
-            }else if (bytte==10){
-                out.write((char)10);
+        FileOutputStream out = new FileOutputStream("output4.txt");
+        int read = file.read();
+        PrintWriter writer = new PrintWriter(out);
+        while (read != -1) {
+            if(read==' '){
+                writer.print(' ');
+            }else if(read == 10){
+                writer.print((char)10);
             }else{
-                for (int i = 0; i <digits.length() ; i++) {
-                    out.write(digits.charAt(i));
-                }
+                writer.print(read);
             }
-            bytte = file.read();
+
+            read = file.read();
         }
+        writer.flush();
         file.close();
         out.close();
     }
