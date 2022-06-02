9d29d9586428d9c20818b78cc1eca72f11badf25
TeodorGjava <90547780+TeodorGjava@users.noreply.github.com>
2022-06-02 22:12:04 +0300

    predicateAndComparator

---
+++ src/FunctionalProgramming/Exc/FindTheSmallest.java
@@ -0,0 +1,37 @@
+package FunctionalProgramming.Exc;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.function.Function;
+import java.util.function.UnaryOperator;
+import java.util.stream.Collectors;
+
+public class FindTheSmallest {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        List<Integer> list = Arrays.stream(sc.nextLine().split(" "))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+        Function<List<Integer>, Integer> func = integerList -> {
+            int minElement = integerList.stream().mapToInt(e -> e).min().getAsInt();
+            return integerList.lastIndexOf(minElement);
+        };
+        System.out.println(func.apply(list));
+    }
+
+
+    //method
+
+    public static Integer minEIndex(List<Integer> list) {
+        int min = Integer.MAX_VALUE;
+        int minIndex = -1;
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i) <= min) {
+                min = list.get(i);
+                minIndex = i;
+            }
+        }
+        return minIndex;
+    }
+}
