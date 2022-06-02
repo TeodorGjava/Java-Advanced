ea0e47cc17903467c563247c370e270f83df06a1
TeodorGjava <90547780+TeodorGjava@users.noreply.github.com>
2022-06-02 20:04:50 +0300

    Update SortEven.java

--- src/FunctionalProgramming/SortEven.java
+++ src/FunctionalProgramming/SortEven.java
@@ -3,20 +3,24 @@ package FunctionalProgramming;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
+import java.util.function.Function;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class SortEven {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        Predicate<Integer> isEven = e-> e%2==0;
         List<Integer> numbs = Arrays.stream(sc.nextLine().split(", "))
                 .map(Integer::parseInt)
+                .filter(isEven)
                 .collect(Collectors.toList());
-        numbs.removeIf(n -> n % 2 != 0);
         String collect = numbs.stream().map(String::valueOf).collect(Collectors.joining(", "));
         System.out.println(collect);
         numbs.sort(Integer::compareTo);
         collect = numbs.stream().map(String::valueOf).collect(Collectors.joining(", "));
         System.out.println(collect);
     }
+
 }
+
