9bd3d5c84c0213a1db7038cb6d72b5404135163f
TeodorGjava <90547780+TeodorGjava@users.noreply.github.com>
2022-06-02 20:45:08 +0300

    excercise

---
+++ src/FunctionalProgramming/Exc/CustomMinFunc.java
@@ -0,0 +1,28 @@
+package FunctionalProgramming.Exc;
+
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.function.Function;
+
+public class CustomMinFunc {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] numbers = Arrays.stream(sc.nextLine().split(" "))
+                .mapToInt(Integer::parseInt).toArray();
+        //Min Functions
+        //stream type
+        Function<int[], Integer> streamFunction = arr-> Arrays.stream(arr).min().getAsInt();
+        // method type
+        Function<int[], Integer> smallest = (int[] arr) -> {
+            int minValue = Integer.MAX_VALUE;
+            for (int e : arr
+            ) {
+                if (e < minValue) {
+                    minValue = e;
+                }
+            }
+            return minValue;
+        };
+        System.out.println(smallest.apply(numbers));
+    }
+}
