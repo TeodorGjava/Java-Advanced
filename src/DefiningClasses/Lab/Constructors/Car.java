6874d8ba4c786458a524f526cbe51acbf510f6d3
TeodorGjava <90547780+TeodorGjava@users.noreply.github.com>
2022-06-03 20:40:42 +0300

    Different constructors

---
+++ src/DefiningClasses/Lab/Constructors/Car.java
@@ -0,0 +1,34 @@
+package DefiningClasses.Lab.Constructors;
+
+public class Car {
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getHp() {
+        return hp;
+    }
+
+    String brand;
+    String model;
+    int hp;
+
+    public Car(String brand) {
+        this.brand = brand;
+        this.model="unknown";
+        this.hp =-1;
+    }
+
+    public Car(String brand, String model, int hp) {
+        this.brand = brand;
+        this.model = model;
+        this.hp = hp;
+    }
+    public String carInfo(){
+        return String.format("The car is: %s %s - %d HP.",this.getBrand(),this.getModel(),this.getHp());
+    }
+}
