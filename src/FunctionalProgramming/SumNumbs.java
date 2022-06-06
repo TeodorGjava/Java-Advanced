6626dddb1ba5a350ad95d09dea8133c08eb96e03
Teodor Georgiev <tgeorgievmk2@gmail.com>
2022-06-06 13:34:17 +0300

    Create SumNumbs.java

---
+++ src/FunctionalProgramming/SumNumbs.java
@@ -0,0 +1,20 @@
+package FunctionalProgramming;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.function.Function;
+import java.util.function.UnaryOperator;
+import java.util.stream.Collectors;
+
+public class SumNumbs {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        List<Integer> numbs = Arrays.stream(sc.nextLine().split(", "))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+        System.out.println("Count = "+ numbs.size());
+        System.out.println("Sum = "+numbs.stream().mapToInt(e->e).sum());
+        
+    }
+}
