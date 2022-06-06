ea00f5f6522cdeda35726550229cc9d9a4cc6a98
Teodor Georgiev <tgeorgievmk2@gmail.com>
2022-06-06 14:43:23 +0300

    Create FindEvenOrOdd.java

---
+++ src/FunctionalProgramming/Lab/FindEvenOrOdd.java
@@ -0,0 +1,27 @@
+package FunctionalProgramming.Lab;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.function.Predicate;
+
+public class FindEvenOrOdd {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] input = sc.nextLine().split(" ");
+        int a = Integer.parseInt(input[0]);
+        int b = Integer.parseInt(input[1]);
+        List<Integer> numbersList = new LinkedList<>();
+        Predicate<Integer> isEven = e-> e%2==0;
+        Predicate<Integer> isOdd = e-> e%2==1;
+        for (int i = a; i <=b ; i++) {
+            numbersList.add(i);
+        }
+        String type = sc.nextLine();
+        if(type.equals("even")){
+            numbersList.stream().filter(isEven).forEach(x-> System.out.print(x+" "));
+        }else{
+            numbersList.stream().filter(isOdd).forEach(x-> System.out.print(x+" "));
+        }
+    }
+}
