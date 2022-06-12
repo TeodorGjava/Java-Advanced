e40e6cf105a76d101ad289ec6aca896fea1de4c2
TeodorGjava <90547780+TeodorGjava@users.noreply.github.com>
2022-06-12 15:36:24 +0300

    Generics

---
+++ src/Generics/Main.java
@@ -0,0 +1,14 @@
+package Generics;
+
+public class Main {
+    public static void main(String[] args) {
+        Jar<Integer> jar = new Jar<>();
+        jar.add(3);
+        jar.add(5);
+        jar.add(7);
+        jar.remove();
+        System.out.println();
+    }
+
+
+}
