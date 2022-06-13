1bd203161ce0c0edcc60bba06c9d9ff3d2f477dc
TeodorGjava <90547780+TeodorGjava@users.noreply.github.com>
2022-06-13 21:12:10 +0300

    CustomList Functional

--- src/Genn/CustomList.java
+++ src/Genn/CustomList.java
@@ -1,4 +1,45 @@
 package Genn;
 
-public class CustomList {
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class CustomList<T extends Comparable<T>> {
+    private List<T> values;
+
+    public CustomList() {
+        this.values=new ArrayList<>();
+    }
+
+    public void add(T element){
+        values.add(element);
+    }
+    public T remove(int index){
+        return values.remove(index);
+    }
+    public boolean contains(T element){
+        return values.contains(element);
+    }
+    public void swap(int firstIndex,int secondIndex){
+        Collections.swap(values,firstIndex,secondIndex);
+    }
+    public long greaterThan(T element){
+        return values.stream().filter(e->e.compareTo(element)>0).count();
+    }
+    public T getMin(){
+        return values.stream().min(Comparable::compareTo).get();
+    }
+    public T getMax(){
+        return values.stream().max(Comparable::compareTo).get();
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder str = new StringBuilder();
+        for (T val: values
+             ) {
+            str.append(String.format("%s%n",val.toString()));
+        }
+        return str.toString();
+    }
 }
---
+++ src/Genn/Sorter.java
@@ -0,0 +1,8 @@
+package Genn;
+
+public class Sorter {
+    public static <T extends Comparable<T>> void sort(CustomList<T> list){
+        //bubble sort
+        
+    }
+}
