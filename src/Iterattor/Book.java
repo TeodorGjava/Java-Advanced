a7eb4d82649e67ebd74114ba88f208a7e3482bdb
TeodorGjava <90547780+TeodorGjava@users.noreply.github.com>
2022-06-14 19:22:25 +0300

    iterable

---
+++ src/Iterattor/Book.java
@@ -0,0 +1,49 @@
+package Iterattor;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Book {
+    private String title;
+    private int year;
+    private List<String> authors;
+
+    public Book(String title, int year, String... authors) {
+        setTitle(title);
+        setYear(year);
+        setAuthors();
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public void setAuthors(String... authors) {
+        this.authors = Arrays.asList(authors);
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public List<String> getAuthors() {
+        return authors;
+    }
+
+    public void setTitle(String... newTitle) {
+        if (newTitle.length == 0) {
+            System.out.println("!");
+        } else {
+            this.authors = Arrays.asList(newTitle);
+        }
+    }
+}
---
+++ src/Iterattor/Main.java
@@ -0,0 +1,19 @@
+package Iterattor;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Book book1 = new Book("Animal farm", 2003, "George Orwell");
+        Book book2 = new Book("Animal farm", 2003, "George Orwell", "Robert");
+        Book book3 = new Book("Animal farm", 2003);
+        List<Book> bookList = new ArrayList<>();
+        bookList.add(book1);
+        bookList.add(book2);
+        bookList.add(book3);
+      
+    }
+}
