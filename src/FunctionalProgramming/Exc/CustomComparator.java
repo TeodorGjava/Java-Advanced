6ebf8e02bb25aca7e5ef627d2c7e43882b4d2ca5
Teodor Georgiev <tgeorgievmk2@gmail.com>
2022-06-14 14:37:21 +0300

    Update CustomComparator.java

--- src/FunctionalProgramming/Exc/CustomComparator.java
+++ src/FunctionalProgramming/Exc/CustomComparator.java
@@ -7,13 +7,14 @@ import java.util.Scanner;
 public class CustomComparator {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        Integer[] nums = Arrays.stream(sc.nextLine().split(" ")).map(Integer::parseInt).toArray(Integer[]::new);
+        Integer[] nums = Arrays.stream(sc.nextLine().split(" "))
+                .map(Integer::parseInt).toArray(Integer[]::new);
         Comparator<Integer> comparator = (num1, num2)->{
             if(num1%2==0&&num2%2!=0){
                 //n1- even
                 //n2-odd
                 return -1;
-            }else if(num1%2==1&&num2%2==0){
+            }else if(num1%2!=0&&num2%2==0){
                 //opposite
                 return 1;
             }else{
