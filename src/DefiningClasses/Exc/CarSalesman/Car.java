794684641039dc98f8d84db961c0b192984d6456
Teodor Georgiev <tgeorgievmk2@gmail.com>
2022-06-15 12:18:47 +0300

    Update Car.java

--- src/DefiningClasses/Exc/CarSalesman/Car.java
+++ src/DefiningClasses/Exc/CarSalesman/Car.java
@@ -2,48 +2,39 @@ package DefiningClasses.Exc.CarSalesman;
 
 public class Car {
     //a model, engine, weight, and color.
-    String model;
-    Engine engine;
-    String weight;
-    String color;
 
-    public Car(String model, Engine engine, String color) {
-        this.model = model;
-        this.engine = engine;
-        this.color = color;
-        this.weight = "n/a";
-    }
+        public String getColor() {
+            return this.color;
+        }
 
-    public Car(Engine engine, String model, String weight) {
-        this.model = model;
-        this.engine = engine;
-        this.weight = weight;
-        this.color = "n/a";
-    }
+        public String getWeight() {
+            return this.weight;
+        }
 
-    public Car(String model, Engine engine) {
-        this.model = model;
-        this.engine = engine;
-        this.weight = "n/a";
-        this.color = "n/a";
-    }
+        public Engine getEngine() {
+            return this.engine;
+        }
 
-    public Car(String model, Engine engine, String weight, String color) {
-        this.model = model;
-        this.engine = engine;
-        this.weight = weight;
-        this.color = color;
-    }
+        public String getModel() {
+            return this.model;
+        }
 
-    @Override
-    public String toString() {
-        return String.format("%s:%n" +
-                        "%s:%n" +
-                        "Power: %s%n" +
-                        "Displacement: %s%n" +
-                        "Efficiency: %s%n" +
-                        "Weight: %s%n" +
-                        "Color: %s%n", this.model, this.engine.model, this.engine.power, this.engine.displacement,
-                this.engine.efficiency, this.weight, this.color);
+        public Car(String model, Engine engine, String weight, String color) {
+            this.model = model;
+            this.engine = engine;
+            this.weight = weight;
+            this.color = color;
+        }
+
+        private String model;
+        private Engine engine;
+        private String weight;
+        private String color;
+
+        @Override
+        public String toString() {
+            return this.model + ":" +"\n"+ String.format("%s" +
+                    "  Weight: %s%n" +
+                    "  Color: %s",this.engine,this.weight,this.color);
+        }
     }
-}
