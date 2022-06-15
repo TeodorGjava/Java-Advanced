3a96387daa047d41a63aef25a38652fe49e49915
Teodor Georgiev <tgeorgievmk2@gmail.com>
2022-06-15 12:18:44 +0300

    Update Engine.java

--- src/DefiningClasses/Exc/CarSalesman/Engine.java
+++ src/DefiningClasses/Exc/CarSalesman/Engine.java
@@ -1,37 +1,42 @@
 package DefiningClasses.Exc.CarSalesman;
 
 public class Engine {
-    //model, power, displacement, and efficiency
-    String model;
-    String power;
-    String displacement = "n/a";
-    String efficiency;
 
-    public Engine(String model, String power) {
-        this.model = model;
-        this.power = power;
-        this.displacement="n/a";
-        this.efficiency = "n/a";
+
+        public String getEfficiency() {
+            return this.efficiency;
+        }
+
+        public String getDisplacement() {
+            return this.displacement;
+        }
+
+        public String getPower() {
+            return this.power;
+        }
+
+        public String getEngineModel() {
+            return this.engineModel;
+        }
+
+        public Engine(String engineModel, String power, String displacement, String efficiency) {
+            this.engineModel = engineModel;
+            this.power = power;
+            this.displacement = displacement;
+            this.efficiency = efficiency;
+        }
+
+        @Override
+        public String toString() {
+            return "  " + this.engineModel + ":" + "\n"+String.format(
+                    "    Power: %s%n" +
+                            "    Displacement: %s%n" +
+                            "    Efficiency: %s%n", this.power, this.displacement, this.efficiency);
+        }
+
+        private String engineModel;
+        private String power;
+        private String displacement;
+        private String efficiency;
 
     }
-
-    public Engine(String model, String power, int displacement) {
-        this.model = model;
-        this.power = power;
-        this.displacement = String.valueOf(displacement);
-        this.efficiency= "n/a";
-    }
-
-    public Engine(String model, String power, String displacement, String efficiency) {
-        this.model = model;
-        this.power = power;
-        this.displacement = displacement;
-        this.efficiency = efficiency;
-    }
-    public Engine(String model, String power,String efficiency) {
-        this.model = model;
-        this.power = power;
-        this.efficiency = efficiency;
-        this.displacement="n/a";
-    }
-}
