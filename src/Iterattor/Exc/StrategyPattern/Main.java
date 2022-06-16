00b63161d2c16e669926ac121507491d9ac1485a
TeodorGjava <90547780+TeodorGjava@users.noreply.github.com>
2022-06-16 22:09:21 +0300

    StrategyPattern example

---
+++ src/Iterattor/Exc/StrategyPattern/Main.java
@@ -0,0 +1,27 @@
+package Iterattor.Exc.StrategyPattern;
+
+import java.util.Scanner;
+import java.util.TreeSet;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = Integer.parseInt(sc.nextLine());
+        TreeSet<Person> byName = new TreeSet<>(new AgeComparator());
+        TreeSet<Person> byAge = new TreeSet<>(new NameComparator());
+        for (int i = 0; i < n; i++) {
+            String[] peopleInfo = sc.nextLine().split(" ");
+            String name = peopleInfo[0];
+            int age = Integer.parseInt(peopleInfo[1]);
+            Person person = new Person(name,age);
+            byAge.add(person);
+            byName.add(person);
+        }
+        for(Person person : byAge){
+            System.out.print(person);
+        }
+        for(Person person: byName){
+            System.out.print(person);
+        }
+    }
+}
