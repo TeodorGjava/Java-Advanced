c8eef283dab813b2444d3fe0a2ae91c182be9bd4
TeodorGjava <90547780+TeodorGjava@users.noreply.github.com>
2022-06-21 22:04:46 +0300

    Create SortEven.java

---
+++ src/FunctionalProgramming/Lab/SortEven.java
@@ -0,0 +1,26 @@
+package FunctionalProgramming.Lab;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+public class SortEven {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Predicate<Integer> isEven = e-> e%2==0;
+        List<Integer> numbs = Arrays.stream(sc.nextLine().split(", "))
+                .map(Integer::parseInt)
+                .filter(isEven)
+                .collect(Collectors.toList());
+        String collect = numbs.stream().map(String::valueOf).collect(Collectors.joining(", "));
+        System.out.print(collect);
+        numbs.sort(Integer::compareTo);
+        collect = numbs.stream().map(String::valueOf).collect(Collectors.joining(", "));
+        System.out.println(collect);
+    }
+
+}
+
