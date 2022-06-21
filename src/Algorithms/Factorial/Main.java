b73e6c469d31129f6edeba1468f42714b9fbaebd
TeodorGjava <90547780+TeodorGjava@users.noreply.github.com>
2022-06-21 21:21:50 +0300

    Create Main.java

---
+++ src/Algorithms/Factorial/Main.java
@@ -0,0 +1,19 @@
+package Algorithms.Factorial;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int n = Integer.parseInt(sc.nextLine());
+        System.out.println(fact(n));
+    }
+
+    public static long fact(int n) {
+        if (n <= 1) {
+            return 1;
+        }
+        return n * fact(n - 1);
+    }
+}
