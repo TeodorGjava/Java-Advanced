56e2fae3dfd9f1be3b2e2f72891d6af24219d5fd
TeodorGjava <90547780+TeodorGjava@users.noreply.github.com>
2022-06-24 11:06:50 +0300

    Update HandsOfCards.java

--- src/SetsAndMapsAdvanced/exc/HandsOfCards.java
+++ src/SetsAndMapsAdvanced/exc/HandsOfCards.java
@@ -5,86 +5,99 @@ import java.util.*;
 public class HandsOfCards {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String[] input = sc.nextLine().split(":");
-        Map<String, Set<String>> playersAndCards = new TreeMap<>();
+        String[] input = sc.nextLine().split(": ");
+        Map<String, Set<String>> playersAndCards = new LinkedHashMap<>();
         while (!input[0].equals("JOKER")) {
             String name = input[0];
             String[] cards = input[1].split(", ");
             playersAndCards.putIfAbsent(name, new LinkedHashSet<>());
-            for (int i = 0; i < cards.length ; i++) {
-                if(!playersAndCards.get(name).contains(cards[i])){
+            for (int i = 0; i < cards.length; i++) {
+                if (!playersAndCards.get(name).contains(cards[i])) {
                     playersAndCards.get(name).add(cards[i]);
                 }
             }
-            input = sc.nextLine().split(":");
+            input = sc.nextLine().split(": ");
         }
-        final int[] sum = {0};
         for (var entry : playersAndCards.entrySet()
         ) {
+        final int[] sum = {0};
+
             final int[] result = {0};
-            for (int i = 0; i < entry.getValue().size(); i++) {
                 entry.getValue().forEach(x -> {
-                    switch (x.substring(0, x.length() - 1)) {
-                        case "2":
+                    switch (x.charAt(0)) {
+                        case '1':
+                            result[0]=10;
+                            switch (x.charAt((2))){
+                                case 'S':
+                                    result[0] = result[0] * 4;
+                                    break;
+                                case 'H':
+                                    result[0] = result[0] * 3;
+                                    break;
+                                case 'D':
+                                    result[0] = result[0] * 2;
+                                    break;
+                                case 'C':
+                                    break;
+                            }
+                            break;
+                        case '2':
                             result[0] = 2;
                             break;
-                        case "3":
+                        case '3':
                             result[0] = 3;
                             break;
-                        case "4":
+                        case '4':
                             result[0] = 4;
                             break;
-                        case "5":
+                        case '5':
                             result[0] = 5;
                             break;
-                        case "6":
+                        case '6':
                             result[0] = 6;
                             break;
-                        case "7":
+                        case '7':
                             result[0] = 7;
                             break;
-                        case "8":
+                        case '8':
                             result[0] = 8;
                             break;
-                        case "9":
+                        case '9':
                             result[0] = 9;
                             break;
-                        case "10":
-                            result[0] = 10;
-                            break;
-                        case "J":
+                        case 'J':
                             result[0] = 11;
                             break;
-                        case "Q":
+                        case 'Q':
                             result[0] = 12;
                             break;
-                        case "K":
+                        case 'K':
                             result[0] = 13;
                             break;
-                        case "A":
+                        case 'A':
                             result[0] = 14;
                             break;
                     }
-                    switch (x.substring(x.length() - 1)) {
-                        case "S":
+                    switch (x.charAt(1)) {
+                        case 'S':
                             result[0] = result[0] * 4;
                             break;
-                        case "H":
+                        case 'H':
                             result[0] = result[0] * 3;
                             break;
-                        case "D":
+                        case 'D':
                             result[0] = result[0] * 2;
                             break;
-                        case "C":
+                        case 'C':
                             break;
                     }
                     sum[0] += result[0];
-                    result[0]=0;
+                    result[0] = 0;
                 });
-            }
-        }
-        for (var entry: playersAndCards.entrySet()){
-            System.out.println(entry.getKey()+": "+sum[0]);
+
+
+                    System.out.println(entry.getKey() + ": " + sum[0]);
+
         }
     }
 }
