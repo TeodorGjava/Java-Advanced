fa08e6be2eed62cb49b3069a0197304834f8d884
TeodorGjava <90547780+TeodorGjava@users.noreply.github.com>
2022-06-24 10:11:03 +0300

    Update WrongMeasurements.java

--- src/MultidimensionalArrays/Lab/WrongMeasurements.java
+++ src/MultidimensionalArrays/Lab/WrongMeasurements.java
@@ -5,98 +5,35 @@ import java.util.Scanner;
 
 public class WrongMeasurements {
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int rows = Integer.parseInt(scanner.nextLine());
-        int[][] matrix = inputMatrix(scanner, rows, "\\s+");
-        int[] indexes = Arrays.stream(scanner.nextLine().split("\\s+"))
-                .mapToInt(Integer::parseInt)
-                .toArray();
-        int number = matrix[indexes[0]][indexes[1]];
-        int[][] outputMatrix = new int[rows][];
-        for (int row = 0; row < rows; row++) {
-            outputMatrix[row] = new int[matrix[row].length];
-            for (int col = 0; col < matrix[row].length; col++) {
-                if (matrix[row][col] == number) {
-                    int up = 0;
-                    int down = 0;
-                    int left = 0;
-                    int right = 0;
-                    if (row == 0) {
-                        if (col == 0) {
-                            right = matrix[row][col + 1] == number ? 0 : matrix[row][col + 1];
-                            if (matrix[row + 1].length == matrix[row].length)
-                                down = matrix[row + 1][col] == number ? 0 : matrix[row + 1][col];
-                        } else if (col == matrix[row].length - 1) {
-                            left = matrix[row][col - 1] == number ? 0 : matrix[row][col - 1];
-                            if (matrix[row + 1].length == matrix[row].length)
-                                down = matrix[row + 1][col] == number ? 0 : matrix[row + 1][col];
-                        } else {
-                            right = matrix[row][col + 1] == number ? 0 : matrix[row][col + 1];
-                            left = matrix[row][col - 1] == number ? 0 : matrix[row][col - 1];
-                            if (matrix[row + 1].length == matrix[row].length)
-                                down = matrix[row + 1][col] == number ? 0 : matrix[row + 1][col];
-                        }
-                    } else if (row == rows - 1) {
-                        if (col == 0) {
-                            right = matrix[row][col + 1] == number ? 0 : matrix[row][col + 1];
-                            if (matrix[row - 1].length == matrix[row].length)
-                                up = matrix[row - 1][col] == number ? 0 : matrix[row - 1][col];
-                        } else if (col == matrix[row].length - 1) {
-                            left = matrix[row][col - 1] == number ? 0 : matrix[row][col - 1];
-                            if (matrix[row - 1].length == matrix[row].length)
-                                up = matrix[row - 1][col] == number ? 0 : matrix[row - 1][col];
-                        } else {
-                            right = matrix[row][col + 1] == number ? 0 : matrix[row][col + 1];
-                            left = matrix[row][col - 1] == number ? 0 : matrix[row][col - 1];
-                            if (matrix[row - 1].length == matrix[row].length)
-                                up = matrix[row - 1][col] == number ? 0 : matrix[row - 1][col];
-                        }
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+
+            int rowCol = Integer.parseInt(sc.nextLine());
+            int[][] matrix = new int[rowCol][];
+
+            for (int i = 0; i < rowCol; i++) {
+                matrix[i] = Arrays.stream(sc.nextLine().split("\\s+")).mapToInt(Integer::parseInt).toArray();
+            }
+            int[][] fixedMatrix = new int[rowCol][matrix[0].length];
+
+            String[] rowColumn = sc.nextLine().split("\\s+");
+            int row = Integer.parseInt(rowColumn[0]);
+            int col = Integer.parseInt(rowColumn[1]);
+
+            int wrongValue = matrix[row][col];
+
+            for (int i = 0; i < rowCol; i++) {
+                for (int j = 0; j < matrix[i].length; j++) {
+                    if (matrix[i][j] == wrongValue) {
+                        fixedMatrix[i][j] = ((j == 0 ? 0 : matrix[i][j - 1] == wrongValue ? 0 : matrix[i][j - 1]) +
+                                (j == matrix[i].length - 1 ? 0 : matrix[i][j + 1] == wrongValue ? 0 : matrix[i][j + 1]) +
+                                (i == 0 ? 0 : matrix[i - 1][j] == wrongValue ? 0 : matrix[i - 1][j]) +
+                                (i == rowCol - 1 ? 0 : matrix[i + 1][j] == wrongValue ? 0 : matrix[i + 1][j]));
                     } else {
-                        if (col == 0) {
-                            right = matrix[row][col + 1] == number ? 0 : matrix[row][col + 1];
-                            if (matrix[row + 1].length == matrix[row].length)
-                                down = matrix[row + 1][col] == number ? 0 : matrix[row + 1][col];
-                            if (matrix[row - 1].length == matrix[row].length)
-                                up = matrix[row - 1][col] == number ? 0 : matrix[row - 1][col];
-                        } else if (col == matrix[row].length - 1) {
-                            left = matrix[row][col - 1] == number ? 0 : matrix[row][col - 1];
-                            if (matrix[row + 1].length == matrix[row].length)
-                                down = matrix[row + 1][col] == number ? 0 : matrix[row + 1][col];
-                            if (matrix[row - 1].length == matrix[row].length)
-                                up = matrix[row - 1][col] == number ? 0 : matrix[row - 1][col];
-                        } else {
-                            right = matrix[row][col + 1] == number ? 0 : matrix[row][col + 1];
-                            left = matrix[row][col - 1] == number ? 0 : matrix[row][col - 1];
-                            if (matrix[row - 1].length == matrix[row].length)
-                                up = matrix[row - 1][col] == number ? 0 : matrix[row - 1][col];
-                            if (matrix[row + 1].length == matrix[row].length)
-                                down = matrix[row + 1][col] == number ? 0 : matrix[row + 1][col];
-                        }
+                        fixedMatrix[i][j] = matrix[i][j];
                     }
-                    int sum = left + right + up + down;
-                    outputMatrix[row][col] = sum;
-                } else outputMatrix[row][col] = matrix[row][col];
+                }
             }
-        }
-        printMatrix(outputMatrix);
-    }
-
-    private static void printMatrix(int[][] outputMatrix) {
-        for (int[] matrix : outputMatrix) {
-            for (int number : matrix) {
-                System.out.print(number + " ");
-            }
-            System.out.println();
+            System.out.println(Arrays.deepToString(fixedMatrix).replace("], ", "\n").replaceAll("[\\[\\],]", ""));
         }
     }
-
-    private static int[][] inputMatrix(Scanner scanner, int rows, String pattern) {
-        int[][] matrix = new int[rows][];
-        for (int row = 0; row < rows; row++) {
-            matrix[row] = Arrays.stream(scanner.nextLine().split(pattern)).mapToInt(Integer::parseInt).toArray();
-        }
-        return matrix;
-
-    }
-}
