394915a62fe677b1793c1339c25673813d6dfec9
TeodorGjava <90547780+TeodorGjava@users.noreply.github.com>
2022-06-28 22:10:06 +0300

    enums

---
+++ src/OOP/workingWithAbstraction/Hotel/Discount.java
@@ -0,0 +1,28 @@
+package OOP.workingWithAbstraction.Hotel;
+
+public enum Discount {
+    VIP(0.8),
+    SECOND_VISIT(0.9),
+    NONE(1);
+
+    private double priceReduction;
+
+    Discount(double priceReduction){
+        this.priceReduction=priceReduction;
+    }
+    public double getPriceReduction(){
+        return priceReduction;
+    }
+    public static Discount parse(String str){
+        switch (str){
+            case"VIP":
+                return VIP;
+            case"SecondVisit":
+                return SECOND_VISIT;
+            case"None":
+                return NONE;
+            default:
+                throw new IllegalStateException("Unknown enum value "+str);
+        }
+    }
+}
