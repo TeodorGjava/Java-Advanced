dda381d857951e9f3a50905cf3af78aa88f2b0b9
TeodorGjava <90547780+TeodorGjava@users.noreply.github.com>
2022-06-28 22:09:37 +0300

    Create HotelCalculator

---
+++ src/OOP/workingWithAbstraction/Hotel/Main.java
@@ -0,0 +1,17 @@
+package OOP.workingWithAbstraction.Hotel;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] line = sc.nextLine().split("\\s+");
+
+        double pricePerDay = Double.parseDouble(line[0]);
+        int days = Integer.parseInt(line[1]);
+        Season season = Season.parse(line[2]);
+
+        Discount discount = Discount.parse(line[3]);
+
+    }
+}
---
+++ src/OOP/workingWithAbstraction/Hotel/PriceCalculator.java
@@ -0,0 +1,20 @@
+package OOP.workingWithAbstraction.Hotel;
+
+public class PriceCalculator {
+
+    private double pricePerDay;
+    private int days;
+    private Season season;
+    private Discount discount;
+
+    public PriceCalculator(double pricePerDay, int number, Season season, Discount discount) {
+        this.pricePerDay = pricePerDay;
+        this.days = number;
+        this.season = season;
+        this.discount = discount;
+    }
+
+    public double calculatePrice() {
+        return pricePerDay * days * season.getMultiplier() * discount.getPriceReduction();
+    }
+}
