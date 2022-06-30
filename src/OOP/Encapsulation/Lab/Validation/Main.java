fada58fad9ad3dd1932f899c0966d6ce59da4715
TeodorGjava <90547780+TeodorGjava@users.noreply.github.com>
2022-06-30 21:23:33 +0300

    OOP - Validation by criteria

---
+++ src/OOP/Encapsulation/Lab/Validation/Main.java
@@ -0,0 +1,35 @@
+package OOP.Encapsulation.Lab.Validation;
+
+import OOP.Encapsulation.Lab.FirstAndReserveTeam.Team;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(reader.readLine());
+        List<Person> people = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            String[] input = reader.readLine().split(" ");
+            try {
+                Person person = new Person();
+                person.setFirstName(input[0]);
+                person.setLastName(input[1]);
+                person.setAge(Integer.parseInt(input[2]));
+                person.setSalary(Double.parseDouble(input[3]));
+                people.add(person);
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        Team team = new Team("Chernite Negri");
+        for (Person person: people
+             ) {
+        }
+
+    }
+}
