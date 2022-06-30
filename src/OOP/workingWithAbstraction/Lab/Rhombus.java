256229a526a8a90fa831663a3fc2b9e9173d37c5
TeodorGjava <90547780+TeodorGjava@users.noreply.github.com>
2022-06-30 18:52:32 +0300

    Lab tasks

---
+++ src/OOP/workingWithAbstraction/Lab/Rhombus.java
@@ -0,0 +1,22 @@
+package OOP.workingWithAbstraction.Lab;
+
+import java.util.Scanner;
+
+public class Rhombus {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int n = Integer.parseInt(sc.nextLine());
+
+        printRhombus(n,n-1);
+
+    }
+
+    private static void printRhombus(int size,int i) {
+        for (int j = 0; j <=size ; j++) {
+            System.out.print(" ");
+        }
+
+    }
+
+}
---
+++ src/OOP/workingWithAbstraction/Lab/pointInRect/Rectangle.java
@@ -0,0 +1,14 @@
+package OOP.workingWithAbstraction.Lab.pointInRect;
+
+public class Rectangle {
+    private Point A;
+    private Point C;
+
+    public Rectangle(Point A, Point C){
+        this.A=A;
+        this.C=C;
+    }
+    public boolean contains(Point x){
+        return x.greaterOrEqual(A)&&x.lessOrEqual(C);
+    }
+}
