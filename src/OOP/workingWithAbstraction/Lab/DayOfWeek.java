45e26099f868c6bc1947d1e5719af5e66e03939b
TeodorGjava <90547780+TeodorGjava@users.noreply.github.com>
2022-06-30 18:51:53 +0300

    Enums

---
+++ src/OOP/workingWithAbstraction/Lab/DayOfWeek.java
@@ -0,0 +1,27 @@
+package OOP.workingWithAbstraction.Lab;
+
+public enum DayOfWeek {
+    MONDAY(1),
+    TUESDAY(2),
+    WEDNESDAY(3),
+    THURSDAY(4),
+    FRIDAY(5),
+    SATURDAY(6),
+    SUNDAY(7);
+
+    private int dayNum;
+
+    private DayOfWeek(int dayNum) {
+        this.dayNum = dayNum;
+    }
+    public int getDayNum(){
+        return this.dayNum;
+    }
+}
+
+class demo {
+    public static void main(String[] args) {
+        DayOfWeek day1 = DayOfWeek.MONDAY;
+        System.out.println(day1);
+    }
+}
