1ec7a16dbaa8d10a7d26f88d68043d0e63147ae1
TeodorGjava <90547780+TeodorGjava@users.noreply.github.com>
2022-07-10 11:26:53 +0300

    Encapsulation exercise

---
+++ src/OOP/Inheritance/Exe/Nfs/CrossMotorcycle.java
@@ -0,0 +1,7 @@
+package OOP.Inheritance.Exe.Nfs;
+
+public class CrossMotorcycle extends Motorcycle {
+    public CrossMotorcycle(double fuel, int horsePower) {
+        super(fuel, horsePower);
+    }
+}
