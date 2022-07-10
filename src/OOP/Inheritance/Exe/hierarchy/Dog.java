b822a4ceca4da762e75d4d13325c38256276860f
TeodorGjava <90547780+TeodorGjava@users.noreply.github.com>
2022-07-10 13:23:22 +0300

    Animals Inheritance

---
+++ src/OOP/Inheritance/Exe/hierarchy/Dog.java
@@ -0,0 +1,12 @@
+package OOP.Inheritance.Exe.hierarchy;
+
+public class Dog extends Animal{
+    public Dog(String name, int age, String gender) {
+        super(name, age, gender);
+    }
+
+    @Override
+    public String produceSound() {
+        return "Woof!";
+    }
+}
