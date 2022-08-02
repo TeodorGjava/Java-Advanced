5a49eb27790223137f275648749236a277092981
TeodorGjava <90547780+TeodorGjava@users.noreply.github.com>
2022-08-02 21:22:37 +0300

    Update Axe.java

--- src/OOP/UnitTesting/rpg_lab/Axe.java
+++ src/OOP/UnitTesting/rpg_lab/Axe.java
@@ -1,8 +1,8 @@
 package OOP.UnitTesting.rpg_lab;
 
-public class Axe {
+public class Axe implements Weapon {
 
-    private int attackPoints;
+    private final int attackPoints;
     private int durabilityPoints;
 
     public Axe(int attack, int durability) {
@@ -10,15 +10,18 @@ public class Axe {
         this.durabilityPoints = durability;
     }
 
+    @Override
     public int getAttackPoints() {
         return this.attackPoints;
     }
 
+    @Override
     public int getDurabilityPoints() {
         return this.durabilityPoints;
     }
 
-    public void attack(Dummy target) {
+    @Override
+    public void attack(Target target) {
         if (this.durabilityPoints <= 0) {
             throw new IllegalStateException("Axe is broken.");
         }
