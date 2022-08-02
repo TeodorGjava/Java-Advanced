b9ecdff2c125198205990056ce2bbacdb5b4f8fb
TeodorGjava <90547780+TeodorGjava@users.noreply.github.com>
2022-08-02 20:02:08 +0300

    Update NumberInRange.java

--- src/OOP/ExceptionsAndErrorHandling/NumberInRange.java
+++ src/OOP/ExceptionsAndErrorHandling/NumberInRange.java
@@ -1,4 +1,37 @@
 package OOP.ExceptionsAndErrorHandling;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class NumberInRange {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] bounds = Arrays.stream(sc.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+        String number = sc.nextLine();
+
+        System.out.printf("Range: [%s...%s]%n", bounds[0], bounds[1]);
+        int n = 0;
+        while (true) {
+            try {
+                n = Integer.parseInt(number);
+                isValid(n, bounds);
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid number: " + number);
+            }
+
+        }
+
+
+    }
+
+    public static boolean isValid(int number, int[] bounds) {
+        if (number >= bounds[0] && number <= bounds[1]) {
+            System.out.println("Valid number: " + number);
+            return true;
+        } else {
+            System.out.println("Invalid number " + number);
+        }
+        return false;
+    }
 }
