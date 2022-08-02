e3d0ba71620d7a69bef2005f0d8de31b5df04dec
TeodorGjava <90547780+TeodorGjava@users.noreply.github.com>
2022-08-02 21:22:29 +0300

    Create Target.java

---
+++ src/OOP/UnitTesting/rpg_lab/Target.java
@@ -0,0 +1,12 @@
+package OOP.UnitTesting.rpg_lab;
+
+public interface Target {
+
+    int getHealth();
+
+    void takeAttack(int attackPoints);
+
+    int giveExperience();
+
+    boolean isDead();
+}
