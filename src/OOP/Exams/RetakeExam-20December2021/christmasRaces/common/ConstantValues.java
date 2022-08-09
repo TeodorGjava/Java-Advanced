bfd2de96ce0f495c0378d6d83fe50a411c4410aa
TeodorGjava <90547780+TeodorGjava@users.noreply.github.com>
2022-08-09 09:40:14 +0300

    christmasRace Skeleton

---
+++ src/OOP/Exams/RetakeExam-20December2021/christmasRaces/common/ConstantValues.java
@@ -0,0 +1,10 @@
+package christmasRaces.common;
+
+public class ConstantValues {
+    public static final double MUSCLE_CAR_CUBIC_CENTIMETERS = 5000;
+    public static final int MUSCLE_CAR_MIN_HORSEPOWER = 400;
+    public static final int MUSCLE_CAR_MAX_HORSEPOWER = 600;
+    public static final double SPORTS_CAR_CUBIC_CENTIMETERS = 3000;
+    public static final int SPORTS_CAR_MIN_HORSEPOWER = 250;
+    public static final int SPORTS_CAR_MAX_HORSEPOWER = 450;
+}
