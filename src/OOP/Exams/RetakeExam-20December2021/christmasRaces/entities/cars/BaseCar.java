512558dcf1a03bab6fffbf7c2cb38f36a72da531
TeodorGjava <90547780+TeodorGjava@users.noreply.github.com>
2022-08-09 09:40:23 +0300

    Create BaseCar.java

---
+++ src/OOP/Exams/RetakeExam-20December2021/christmasRaces/entities/cars/BaseCar.java
@@ -0,0 +1,45 @@
+package christmasRaces.entities.cars;
+
+public abstract class BaseCar implements Car {
+    private String model;
+    private int horsePower;
+    private double cubicCentimeters;
+
+    protected BaseCar(String model, int horsePower, double cubicCentimeters) {
+        this.model = model;
+        setHorsePower(horsePower);
+        this.cubicCentimeters = cubicCentimeters;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public void setHorsePower(int horsePower) {
+        this.horsePower = horsePower;
+    }
+
+    public void setCubicCentimeters(double cubicCentimeters) {
+        this.cubicCentimeters = cubicCentimeters;
+    }
+
+    @Override
+    public String getModel() {
+        return model;
+    }
+
+    @Override
+    public int getHorsePower() {
+        return horsePower;
+    }
+
+    @Override
+    public double getCubicCentimeters() {
+        return cubicCentimeters;
+    }
+
+    @Override
+    public double calculateRacePoints(int laps) {
+       return cubicCentimeters / horsePower*1.0 * laps;
+    }
+}
