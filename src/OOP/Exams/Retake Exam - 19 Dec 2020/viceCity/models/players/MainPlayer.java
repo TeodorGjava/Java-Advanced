f7197448cc9ae98237f5d99cff4ba4ea640fc88b
TeodorGjava <90547780+TeodorGjava@users.noreply.github.com>
2022-08-13 11:55:26 +0300

    creating players extending BASEPLAYER

---
+++ src/OOP/Exams/Retake Exam - 19 Dec 2020/viceCity/models/players/MainPlayer.java
@@ -0,0 +1,12 @@
+package viceCity.models.players;
+
+
+public class MainPlayer extends BasePlayer {
+    private static final String PLAYER_NAME = "Tommy Vercetti";
+    private static final int LIFE_POINTS = 100;
+
+    public MainPlayer() {
+        super(PLAYER_NAME, LIFE_POINTS);
+    }
+
+}
