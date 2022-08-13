c956d2238c7366b10bc1019b2e8dbc5bbae7b715
TeodorGjava <90547780+TeodorGjava@users.noreply.github.com>
2022-08-13 16:59:09 +0300

    Create LongHouse.java

---
+++ src/OOP/Exams/Exam – 11 December 2021/catHouse/entities/houses/LongHouse.java
@@ -0,0 +1,10 @@
+package catHouse.entities.houses;
+
+public class LongHouse extends BaseHouse {
+    //Has 15 capacity.
+    private static final int CAPACITY = 15;
+
+    public LongHouse(String name) {
+        super(name, CAPACITY);
+    }
+}
