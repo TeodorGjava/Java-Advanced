7aa7fd1f19fcc6ba2983c27e96fa5e7e34552810
TeodorGjava <90547780+TeodorGjava@users.noreply.github.com>
2022-08-13 11:55:09 +0300

    Create BasePlayer.java

---
+++ src/OOP/Exams/Retake Exam - 19 Dec 2020/viceCity/models/players/BasePlayer.java
@@ -0,0 +1,59 @@
+package viceCity.models.players;
+
+import viceCity.models.guns.Gun;
+import viceCity.repositories.interfaces.GunRepository;
+import viceCity.repositories.interfaces.Repository;
+
+import static viceCity.common.ExceptionMessages.*;
+
+public abstract class BasePlayer implements Player {
+    private String name;
+    private int lifePoints;
+    private Repository<Gun> gunRepository;
+
+    public BasePlayer(String name, int lifePoints) {
+       setName(name);
+        setLifePoints(lifePoints);
+        this.gunRepository = new GunRepository();
+    }
+
+    @Override
+    public String getName() {
+        return this.name;
+    }
+
+    @Override
+    public int getLifePoints() {
+        return this.lifePoints;
+    }
+
+    @Override
+    public Repository<Gun> getGunRepository() {
+        return this.gunRepository;
+    }
+
+    private void setName(String name) {
+        if (name == null || name.trim().isEmpty()) {
+            throw new NullPointerException(NAME_NULL);
+        }
+        this.name = name;
+    }
+
+    private void setLifePoints(int lifePoints) {
+        if (lifePoints < 0) {
+            throw new IllegalArgumentException(PLAYER_LIFE_POINTS_LESS_THAN_ZERO);
+        }
+        this.lifePoints = lifePoints;
+    }
+
+    @Override
+    public boolean isAlive() {
+        return this.lifePoints>0;
+    }
+
+
+    @Override
+    public void takeLifePoints(int points) {
+        this.lifePoints=Math.max(0,this.lifePoints-points);
+    }
+}
