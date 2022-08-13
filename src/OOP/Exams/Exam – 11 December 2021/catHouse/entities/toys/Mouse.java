92be4ac75408d4f61f9224344d39ffd4de92df49
TeodorGjava <90547780+TeodorGjava@users.noreply.github.com>
2022-08-13 16:57:18 +0300

    child classes toys

---
+++ src/OOP/Exams/Exam – 11 December 2021/catHouse/entities/toys/Mouse.java
@@ -0,0 +1,10 @@
+package catHouse.entities.toys;
+
+public class Mouse extends BaseToy {
+    private static final int INITIAL_SOFTNESS = 5;
+    private static final int INITIAL_PRICE = 15;
+
+    public Mouse() {
+        super(INITIAL_SOFTNESS, INITIAL_PRICE);
+    }
+}
