f238c055a8e0374d8cc58197170c8d3bb1607cf3
TeodorGjava <90547780+TeodorGjava@users.noreply.github.com>
2022-08-13 22:30:49 +0300

    Update Bear.java

--- src/OOP/Inheritance/Exe/zoo/Bear.java
+++ src/OOP/Inheritance/Exe/zoo/Bear.java
@@ -1,7 +1,12 @@
 package OOP.Inheritance.Exe.zoo;
 
-public class Bear extends Mammal{
-    public Bear(String name) {
-        super(name);
+public class Bear extends Animal implements Sound{
+
+
+
+
+    public Bear(String name, int age, String gender) {
+        super(name, age, gender);
+
     }
 }
